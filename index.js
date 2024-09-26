612a93f1ce9edfa934a4904255dc2f00fe791649
shyye <emma.art.dev@gmail.com>
2024-09-26 15:00:45 +0200

    Fix Extension 1

--- index.js
+++ index.js
@@ -4,51 +4,61 @@ const state = {
       id: "001-beetroot",
       name: "beetroot",
       price: 0.35,
+      type: "vegetable",
     },
     {
       id: "002-carrot",
       name: "carrot",
       price: 0.35,
+      type: "vegetable",
     },
     {
       id: "003-apple",
       name: "apple",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "004-apricot",
       name: "apricot",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "005-avocado",
       name: "avocado",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "006-bananas",
       name: "bananas",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
       price: 0.35,
+      type: "vegetable",
     },
     {
       id: "008-berry",
       name: "berry",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "009-blueberry",
       name: "blueberry",
       price: 0.35,
+      type: "fruit",
     },
     {
       id: "010-eggplant",
       name: "eggplant",
       price: 0.35,
+      type: "vegetable",
     },
   ],
   cart: [],
@@ -58,6 +68,19 @@ const state = {
 const storeItemListUL = document.querySelector(".store--item-list")
 const cartItemListUL = document.querySelector(".cart--item-list")
 const totalCostSpan = document.querySelector(".total-number")
+
+// Filter value
+const filterSelectElement = document.getElementById("filter")
+
+filterSelectElement.addEventListener('change', function() {
+  const filterValue =  filterSelectElement.value
+  if (filterValue === '') {
+    renderStoreItems()
+  } else {
+    let itemList = state.items.filter((item) => item.type === filterValue)
+    renderStoreItems(itemList)
+  }  
+})
 
 // Add to cart
 function addToCart(itemId) {
@@ -100,11 +123,11 @@ function decreaseQuantity(cartItem) {
   renderCartItems();
 }
 
-function renderStoreItems() {
+function renderStoreItems(itemList = state.items) {
   // Reset items
   storeItemListUL.innerHTML = "";
 
-  state.items.forEach((item) => {
+  itemList.forEach((item) => {
     let element = document.createElement("li");
     const htmlString = `
           <div class="store--item-icon">
